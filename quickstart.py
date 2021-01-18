015e491905949207b0cc5dc37efbb7f38f9a8c4e
Hunter Holland <huntholland@gmail.com>
2021-01-17 21:25:26 -0500

    Add courses to for loop

--- quickstart.py
+++ quickstart.py
@@ -3,6 +3,7 @@ import pickle
 import os.path
 import requests
 import os
+from bs4 import BeautifulSoup as bs
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -36,65 +37,43 @@ def main():
 
     service = build('tasks', 'v1', credentials=creds)
 
-    # Call the Tasks API
-    results = service.tasklists().list(maxResults=10).execute()
-    items = results.get('items', [])
-
-    if not items:
-        print('No task lists found.')
-    else:
-        print('Task lists:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['title'], item['id']))
+    # # Call the Tasks API
+    # results = service.tasklists().list(maxResults=10).execute()
+    # items = results.get('items', [])
+    #
+    # if not items:
+    #     print('No task lists found.')
+    # else:
+    #     print('Task lists:')
+    #     for item in items:
+    #         print(u'{0} ({1})'.format(item['title'], item['id']))
 
     # TODO:
     # Get Assignments for the week from Canvas API
-    # token = os.environ['CANVAS_TOKEN']
-    # headers = {'Authorization': 'Bearer ' + token}
-    # r = requests.get(
-    #     'https://uc.instructure.com/api/v1/users/self/upcoming_events',
-    #     headers=headers)
-    # src = r.content
-    # json_response = r.json()
-    # assignments = json_response[0]
-    # print(json_response[1])
-    # print(src[0])
+    token = os.environ['CANVAS_TOKEN']
+    headers = {'Authorization': 'Bearer ' + token}
+    r = requests.get(
+        'https://uc.instructure.com/api/v1/users/self/upcoming_events',
+        headers=headers)
+    src = r.content
+    json_response = r.json()
 
-    # for j in json_response:
-    #     summary = j['title']
-    #     description = j['description']
+    for j in json_response:
+        base = 'https://tasks.googleapis.com/tasks/v1/lists/'
+        acct = 'cW4xQ1ZJb1NKakljd3lpSQ'
+        mgmt = 'S2tsR09tZWNqMFpTWXNRYQ'
+        web = 'Skpya2JzeE1lUE5nTTYxQg'
+        title = j['title']
+        notes = j['description']
+        due = j['due_at']
+        course = j['context_name']
 
-    # Add assignments for the week
-    # event = {
-    #     'summary': 'Google I/O 2015',
-    #     'location': '800 Howard St., San Francisco, CA 94103',
-    #     'description': 'A chance to hear more about Google\'s developer products.',
-    #     'start': {
-    #         'dateTime': '2015-05-28T09:00:00-07:00',
-    #         'timeZone': 'America/Los_Angeles',
-    #     },
-    #     'end': {
-    #         'dateTime': '2015-05-28T17:00:00-07:00',
-    #         'timeZone': 'America/Los_Angeles',
-    #     },
-    #     'recurrence': [
-    #         'RRULE:FREQ=DAILY;COUNT=2'
-    #     ],
-    #     'attendees': [
-    #         {'email': 'lpage@example.com'},
-    #         {'email': 'sbrin@example.com'},
-    #     ],
-    #     'reminders': {
-    #         'useDefault': False,
-    #         'overrides': [
-    #             {'method': 'email', 'minutes': 24 * 60},
-    #             {'method': 'popup', 'minutes': 10},
-    #         ],
-    #     },
-    # }
-    #
-    # event = service.events().insert(calendarId='primary', body=event).execute()
-    # print('Event created: %s' % (event.get('htmlLink')))
+        if 'ACCT MGR DECISIONS' in course:
+            requests.post(f'{base}{acct}/tasks')
+        elif 'MANAGEMENT IN IT' in course:
+            requests.post(f'{base}{mgmt}/tasks')
+        elif 'WEB SERVER APPLICATION DEV' in course:
+            requests.post(f'{base}{web}/tasks')
 
 
 if __name__ == '__main__':
