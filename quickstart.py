7a2f0af98ec2d3c29dfe0a765ed7f980ca337c03
Hunter Holland <huntholland@gmail.com>
2021-01-17 20:19:36 -0500

    Pivot to using Google Tasks

--- quickstart.py
+++ quickstart.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import datetime
 import pickle
 import os.path
 import requests
@@ -9,12 +8,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+SCOPES = ['https://www.googleapis.com/auth/tasks']
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
+    """Shows basic usage of the Tasks API.
+    Prints the title and ID of the first 10 task lists.
     """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -35,42 +34,67 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
+    service = build('tasks', 'v1', credentials=creds)
 
-    # # Call the Calendar API
-    # now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    # print('Getting the upcoming 10 events')
-    # events_result = service.events().list(calendarId='sekrsgjtndhps1tkelsjs1ff8s@group.calendar.google.com', timeMin=now,
-    #                                     maxResults=10, singleEvents=True,
-    #                                     orderBy='startTime').execute()
-    # events = events_result.get('items', [])
-    #
-    # if not events:
-    #     print('No upcoming events found.')
-    # for event in events:
-    #     start = event['start'].get('dateTime', event['start'].get('date'))
-    #     print(start, event['summary'])
+    # Call the Tasks API
+    results = service.tasklists().list(maxResults=10).execute()
+    items = results.get('items', [])
+
+    if not items:
+        print('No task lists found.')
+    else:
+        print('Task lists:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['title'], item['id']))
 
     # TODO:
-    # Check week
-
-
     # Get Assignments for the week from Canvas API
-    token = os.environ['CANVAS_TOKEN']
-    headers = {'Authorization': 'Bearer ' + token}
-    r = requests.get(
-        'https://uc.instructure.com/api/v1/users/self/upcoming_events',
-        headers=headers)
-    json_response = r.json()
-    assignments = json_response[0]
-    for keys, values in assignments.items():
-         print(keys)
+    # token = os.environ['CANVAS_TOKEN']
+    # headers = {'Authorization': 'Bearer ' + token}
+    # r = requests.get(
+    #     'https://uc.instructure.com/api/v1/users/self/upcoming_events',
+    #     headers=headers)
+    # src = r.content
+    # json_response = r.json()
+    # assignments = json_response[0]
+    # print(json_response[1])
+    # print(src[0])
 
+    # for j in json_response:
+    #     summary = j['title']
+    #     description = j['description']
 
     # Add assignments for the week
-
-
-
+    # event = {
+    #     'summary': 'Google I/O 2015',
+    #     'location': '800 Howard St., San Francisco, CA 94103',
+    #     'description': 'A chance to hear more about Google\'s developer products.',
+    #     'start': {
+    #         'dateTime': '2015-05-28T09:00:00-07:00',
+    #         'timeZone': 'America/Los_Angeles',
+    #     },
+    #     'end': {
+    #         'dateTime': '2015-05-28T17:00:00-07:00',
+    #         'timeZone': 'America/Los_Angeles',
+    #     },
+    #     'recurrence': [
+    #         'RRULE:FREQ=DAILY;COUNT=2'
+    #     ],
+    #     'attendees': [
+    #         {'email': 'lpage@example.com'},
+    #         {'email': 'sbrin@example.com'},
+    #     ],
+    #     'reminders': {
+    #         'useDefault': False,
+    #         'overrides': [
+    #             {'method': 'email', 'minutes': 24 * 60},
+    #             {'method': 'popup', 'minutes': 10},
+    #         ],
+    #     },
+    # }
+    #
+    # event = service.events().insert(calendarId='primary', body=event).execute()
+    # print('Event created: %s' % (event.get('htmlLink')))
 
 
 if __name__ == '__main__':
